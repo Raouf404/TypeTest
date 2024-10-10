f6ab4fc925ed365065da4c2ec666e7928339dc64
Raouf404 <roaufdz222@gmail.com>
2024-10-10 17:09:35 +0100

    Fixed the modal size

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,7 +15,7 @@ const Modal: React.FC<ModalProps> = ({ onClick }) => {
         onClick={onClick}
         className="h-screen w-screen bg-darkGray75 absolute"
       ></div>
-      <div className="flex flex-col items-center p-6 w-[324px] bg-beige absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-[32px]">
+      <div className="flex flex-col items-center p-6 w-[428px] bg-beige absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-[32px]">
         <h2 className="text-2xl mb-12">Choissez votre test</h2>
         <div className="flex flex-col gap-4 mb-8 w-full">
           <div className="flex justify-between items-center w-full">
@@ -25,7 +25,7 @@ const Modal: React.FC<ModalProps> = ({ onClick }) => {
               id="time"
               onChange={(e) => setTime(Number(e.target.value))}
               value={time}
-              className="p-4 rounded-lg w-[138px] bg-beige text-darkGray border border-darkGray"
+              className="p-4 rounded-lg w-[280px] bg-beige text-darkGray border border-darkGray"
             >
               <option value={30}>30 secondes</option>
               <option value={60}>1 minute</option>
@@ -42,7 +42,7 @@ const Modal: React.FC<ModalProps> = ({ onClick }) => {
               id="difficulty"
               onChange={(e) => setDifficulty(e.target.value)}
               value={difficulty}
-              className="p-4 rounded-lg w-[138px] bg-beige text-darkGray border border-darkGray"
+              className="p-4 rounded-lg w-[280px] bg-beige text-darkGray border border-darkGray"
             >
               <option value="easy">Facile</option>
               <option value="medium">Moyen</option>
